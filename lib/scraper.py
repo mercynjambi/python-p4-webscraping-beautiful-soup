08cab1f795773d55be6d072111127ea4a976f19d
mercynjambi <njambim695@gmail.com>
2024-08-21 20:22:53 +0300

    passed tests

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,20 @@
 from turtle import ht
+
 from bs4 import BeautifulSoup
 import requests
 
+# Headers to mimic a browser request
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+
+# Send a GET request
+response = requests.get("https://flatironschool.com/our-courses/", headers=headers)
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Select and print course names
+courses = soup.select('.heading-60-black.color-black.mb-20')
+for course in courses:
+    print(course.get_text(strip=True))
+
+
